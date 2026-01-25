e446a5b8ff303efc46802189f44194cff6b6baba
Caleb Cameron Calvin <calvincaleb09@gmail.com>
2026-01-25 13:43:41 +0200

    login from tweaks

--- inventory-system/src/pages/LoginForm.jsx
+++ inventory-system/src/pages/LoginForm.jsx
@@ -19,7 +19,8 @@ const LoginForm = () => {
                     <div>
                         <label for="email" className="text-left block text-sm/6 font-medium text-gray-800">Email address</label>
                         <div className="mt-2">
-                            <input id="email" placeholder="Password" type="email" name="email" required autocomplete="email" className="border-solid block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="email" placeholder="Email " type="email" name="email" required autocomplete="email" 
+                            className="border border-gray-300  block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
@@ -28,7 +29,7 @@ const LoginForm = () => {
                         <input
                             type={show ? "text" : "password"}
                             placeholder="Password"
-                            className="border-solid block w-full rounded-md bg-white/5 px-3 py-1.5 pr-10 text-white placeholder:text-gray-500 outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                            className="border border-gray-300 block w-full rounded-md bg-white/5 px-3 py-1.5 pr-10 text-white placeholder:text-gray-500 outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                         />
 
                         <div
